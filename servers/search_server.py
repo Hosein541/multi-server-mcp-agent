009d893e2d3f670c53045bb13948b982fd32dbd5
Hosein541 <huseinmohammadi83@gmail.com>
2026-06-29 19:49:21 +0330

    edit search server

--- servers/search_server.py
+++ servers/search_server.py
@@ -1,13 +1,11 @@
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-# load_dotenv()
 ROOT_DIR = Path(__file__).resolve().parent.parent
-
 load_dotenv(ROOT_DIR / ".env")
+
 from mcp.server.fastmcp import FastMCP
 from tavily import TavilyClient
-# from langchain_community.tools.tavily_search import TavilySearchResults
 
 mcp = FastMCP("Search Server")
 
@@ -26,14 +24,11 @@ def search_web(query: str, max_results: int = 5) -> dict:
     Returns:
         Search summary and relevant search results.
     """
-    # query = "tell me abou Iran's today news"
     response = client.search(
         query=query,
         max_results=max_results,
-        # include_answer=True,
-        # include_raw_content=False,
+        
     )
-    # print(f"tavily result:\t{response}")
 
     return {
         "query": query,
